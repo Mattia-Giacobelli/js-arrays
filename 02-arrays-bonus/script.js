1508200062df41a56201aa5ee4b81b8b91d5dec8
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-08 15:44:49 +0200

    fix bonus 1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,7 +10,13 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.toReversed();
+const reversedTeachers = [];
+
+for (let i = teachers.length - 1; i >= 0; i--) {
+  const teacher = teachers[i];
+
+  reversedTeachers.push(teacher)
+}
 console.log(reversedTeachers);
 
 
@@ -24,12 +30,12 @@ for (let i = 0; i < teachers.length; i++) {
   if (name.length >= 5) {
     longNames.push(name)
   }
-  
+
 }
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(1,1)
+teachers.splice(1, 1)
 console.log(teachers);
 
 
